051397f759100a8da3cb2d6bf5169efc314c4c85
German <germanpalero@gmail.com>
2021-10-28 17:12:21 +0200

    fixes

--- src/pages/Welcome/WelcomePage.jsx
+++ src/pages/Welcome/WelcomePage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import myvood from "../../assets/images/voodgreytext.png";
+import myvood from "../../assets/images/myvood.png";
 import { useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import { useForm } from "react-hook-form";
 import { addUserToDatabase } from "../../redux/userData/userData.actions";
+import "./WelcomePage.css"
 
 const WelcomePage = ({ addUserToDatabase, user }) => {
   const history = useHistory();
@@ -25,21 +26,21 @@ const WelcomePage = ({ addUserToDatabase, user }) => {
   };
 
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full h-screen flex justify-center items-center welcomeForm">
       <form
         className="flex flex-col sm:border border-yellow-300 p-8 rounded-md relative"
         onSubmit={handleSubmit(onSubmit)}
       >
         <div
-          className="absolute right-12 text-2xl text-gray-500 cursor-pointer"
+          className="absolute right-12 text-2xl text-white cursor-pointer"
           onClick={() => cancelData()}
         >
           x
         </div>
         <div className="flex justify-center items-center p-2">
-          <img src={myvood} className="w-14" alt="myVood" />
+          <img src={myvood} className="w-24" alt="myVood" />
         </div>
-        <div className="text-sm py-4">
+        <div className="text-sm py-6 text-white font-bold">
           Hey there! We are about to launch, would you like to receive our
           emails?
         </div>
@@ -48,35 +49,35 @@ const WelcomePage = ({ addUserToDatabase, user }) => {
           id="name"
           placeholder="Name"
           {...register("name")}
-          className="text-center p-2 border border-black m-2"
+          className="text-center p-2 border border-white m-2 bg-transparent"
         />
         <input
           type="text"
           id="surname"
           placeholder="Surname"
           {...register("surname")}
-          className="text-center p-2 border border-black m-2"
+          className="text-center p-2 border border-white m-2 bg-transparent"
         />
         <input
           type="text"
           id="email"
           placeholder="E-mail"
           {...register("email")}
-          className=" text-center p-2 border border-black m-2"
+          className=" text-center p-2 border border-white m-2 bg-transparent"
         />
         <input
           type="submit"
           id="submit"
           value="Send"
-          className="p-2 border border-black m-2"
+          className="p-2 m-2 bg-transparent border-2 border-white text-white"
         />
 
-        <div className="flex flex-row p-2">
+        <div className="flex flex-row p-2 items-center justify-center">
           <input type="checkbox" id="checkbox" />
-          <p className="text-sm p-2">
+          <p className="text-xs p-2 yellow">
             {" "}
             I have read and understood MyVood's{" "}
-            <a href="www.google.com" className="text-yellow-500">
+            <a href="www.google.com" className="blue">
               privacy policy
             </a>{" "}
           </p>
