6928e96db3a34a5d294fe28a6d0c38e198a60193
German <germanpalero@gmail.com>
2021-11-11 10:19:45 +0100

    transition plans revisited

--- src/pages/transitionPlans/TransitionPlans.jsx
+++ src/pages/transitionPlans/TransitionPlans.jsx
@@ -1,44 +1,65 @@
 import React from "react";
-
+import "./TransitionPlans.css"
+import myvood from "../../assets/images/myvood.png"
 export default function TransitionPlans() {
   return (
     <div>
       <section className="flex flex-wrap animation landingPage min-w-screen section2">
         <div className="w-full text m-2 mt-4 mx-4 mt-12">
           <p className="text-xs font-bold ">MY JOURNEY, MY LIFE, MY VOOD</p>
-          <h1 className="text-3xl">Transition plans</h1>
+          <h1 className="text-4xl">Transition plans</h1>
         </div>
-        <div className="yellow-bg w-full text-center text-white p-10 m-2 mt-4 mx-4">
-          <h1 className="font-bold">WEST AFRICAN PLAN</h1>
-          <img
-            src="https://static.wixstatic.com/media/fb7254_538501218d2142d88e12cbd80b973ba9~mv2.png/v1/fill/w_150,h_294,al_c,q_85,usm_0.66_1.00_0.01/fb7254_538501218d2142d88e12cbd80b973ba9~mv2.webp"
-            alt=""
+        <div className="yellow-bg w-full text-center text-white p-10 m-2 mt-4 mx-4 flex flex-row items-center text-left">
+        <img
+            src={myvood}
+            className="transition--image"
           />
-          <p>
+          <div className="text-left">
+          <h1 className="font-bold">WEST AFRICAN PLAN</h1>
+          <p className="text-sm">
             Make this yours. Add images, text and links, or connect data from
             your collection.
           </p>
+          </div>
         </div>
-        <div className="blue-bg w-full text-center text-white p-10 m-2 mx-4">
+        <div className="blue-bg w-full text-center text-white p-10 m-2 mt-4 mx-4 flex flex-row items-center text-left">
+        <img
+            src={myvood}
+            className="transition--image"
+          />
+          <div className="text-left">
           <h1 className="font-bold">SOUTH ASIAN PLAN</h1>
-          <p>
+          <p className="text-sm">
             Make this yours. Add images, text and links, or connect data from
             your collection.
           </p>
+          </div>
         </div>
-        <div className="green1-bg w-full text-center text-white p-10 m-2 mx-4">
+        <div className="green1-bg w-full text-center text-white p-10 m-2 mt-4 mx-4 flex flex-row items-center text-left">
+        <img
+            src={myvood}
+            className="transition--image"
+          />
+          <div className="text-left">
           <h1 className="font-bold">MEDITERRANEAN</h1>
-          <p>
+          <p className="text-sm">
             Make this yours. Add images, text and links, or connect data from
             your collection.
           </p>
+          </div>
         </div>
-        <div className="green2-bg w-full text-center text-white p-10 m-2 mb-4 mx-4">
+        <div className="green2-bg w-full text-center text-white p-10 m-2 mt-4 mx-4 flex flex-row items-center text-left">
+        <img
+            src={myvood}
+            className="transition--image"
+          />
+          <div className="text-left">
           <h1 className="font-bold">WORLD FOOD PLAN</h1>
-          <p>
+          <p className="text-sm">
             Make this yours. Add images, text and links, or connect data from
             your collection.
           </p>
+          </div>
         </div>
       </section>
     </div>
